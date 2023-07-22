6d0e3340b34a220cac006dc97fb67d9797c62d95
Ostap <Ostapkriven13@gmail.com>
2023-07-22 12:40:07 +0300

    updait Taimer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,39 +1,78 @@
-const datetimePicker = document.getElementById('datetime-picker');
-const startButton = document.querySelector('[data-start]');
-const daysElement = document.querySelector('[data-days]');
-const hoursElement = document.querySelector('[data-hours]');
-const minutesElement = document.querySelector('[data-minutes]');
-const secondsElement = document.querySelector('[data-seconds]');
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
-let countdownInterval;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
 
-function updateTimerDisplay(days, hours, minutes, seconds) {
-  daysElement.textContent = String(days).padStart(2, '0');
-  hoursElement.textContent = String(hours).padStart(2, '0');
-  minutesElement.textContent = String(minutes).padStart(2, '0');
-  secondsElement.textContent = String(seconds).padStart(2, '0');
-}
-
-function startCountdown() {
-  const targetDate = new Date(datetimePicker.value).getTime();
-
-  countdownInterval = setInterval(() => {
-    const now = new Date().getTime();
-    const distance = targetDate - now;
-
-    if (distance < 0) {
-      clearInterval(countdownInterval);
-      updateTimerDisplay(0, 0, 0, 0);
+    if (selectedDate <= currentDate) {
+      window.alert("Please choose a date in the future");
+      document.querySelector("[data-start]").disabled = true;
     } else {
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      updateTimerDisplay(days, hours, minutes, seconds);
+      document.querySelector("[data-start]").disabled = false;
+    }
+  },
+};
+
+flatpickr("#datetime-picker", options);
+
+let timerInterval;
+
+document.querySelector("[data-start]").addEventListener("click", () => {
+  const selectedDate = new Date(document.querySelector("#datetime-picker").value).getTime();
+  const currentDate = new Date().getTime();
+  const timeDifference = selectedDate - currentDate;
+
+  if (timeDifference <= 0) {
+    window.alert("Please choose a date in the future");
+    return;
+  }
+
+  document.querySelector("[data-start]").disabled = true;
+  document.querySelector("#datetime-picker").disabled = true;
+
+  updateTimerDisplay(timeDifference);
+
+  timerInterval = setInterval(() => {
+    const updatedTimeDifference = selectedDate - new Date().getTime();
+    if (updatedTimeDifference <= 0) {
+      clearInterval(timerInterval);
+      updateTimerDisplay(0);
+      document.querySelector("[data-start]").disabled = false;
+      document.querySelector("#datetime-picker").disabled = false;
+    } else {
+      updateTimerDisplay(updatedTimeDifference);
     }
   }, 1000);
+});
+
+function updateTimerDisplay(ms) {
+  const { days, hours, minutes, seconds } = convertMs(ms);
+  document.querySelector("[data-days]").textContent = addLeadingZero(days);
+  document.querySelector("[data-hours]").textContent = addLeadingZero(hours);
+  document.querySelector("[data-minutes]").textContent = addLeadingZero(minutes);
+  document.querySelector("[data-seconds]").textContent = addLeadingZero(seconds);
 }
 
-startButton.addEventListener('click', () => {
-  startCountdown();
-});
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
