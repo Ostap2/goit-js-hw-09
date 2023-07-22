92ba20ea1eb1967b99b3e567bfbd7488ae4bcb31
Ostap <Ostapkriven13@gmail.com>
2023-07-22 13:12:12 +0300

    іі
    і

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,7 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-
-  } else {
-
-  }
-}
-
-const form = document.querySelector('.form');
-const notiflix = window.notiflix;
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
@@ -24,25 +12,23 @@ function createPromise(position, delay) {
   });
 }
 
-form.addEventListener('submit', (event) => {
+document.querySelector('.form').addEventListener('submit', (event) => {
   event.preventDefault();
-
-  const formData = new FormData(form);
-  const firstDelay = parseInt(formData.get('delay'));
-  const delayStep = parseInt(formData.get('step'));
-  const amount = parseInt(formData.get('amount'));
-
-  notiflix.Notify.removeAll();
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+  
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
 
   for (let i = 0; i < amount; i++) {
-    const delay = firstDelay + i * delayStep;
-
-    createPromise(i + 1, delay)
-      .then(({ position, delay }) => {
-        notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
+    const position = i + 1;
+    createPromise(position, delay).then(({ position, delay }) => {
+      Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({ position, delay }) => {
+      Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    delay += step; 
   }
 });
