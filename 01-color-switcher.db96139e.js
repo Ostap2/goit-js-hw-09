bbe92b6bda50abac2bb7cb90519f940be40c0dbd
Ostap2 <127882524+Ostap2@users.noreply.github.com>
2023-07-24 18:22:59 +0000

    Deploying to gh-pages from @ Ostap2/goit-js-hw-09@70b4ab3f12f65faddef1457d072aa951cc263e5d 🚀

---
+++ 01-color-switcher.db96139e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.db96139e.js.map
