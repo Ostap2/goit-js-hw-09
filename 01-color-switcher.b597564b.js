cf85796506310feeae9ff1a109c8e787b2bd42ed
Ostap2 <127882524+Ostap2@users.noreply.github.com>
2023-07-24 18:16:47 +0000

    Deploying to gh-pages from @ Ostap2/goit-js-hw-09@47f3affced4b9e93a9131e4f23c1823a8533a4cc 🚀

---
+++ 01-color-switcher.b597564b.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),a.addEventListener("click",(function(){e.disabled=!1,a.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.b597564b.js.map
